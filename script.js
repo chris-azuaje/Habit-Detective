c7b4c43c340eaf322fac09791322665f7d41cf59
Chris Azuaje <christopherazuaje@gmail.com>
2024-02-17 09:33:33 -0500

    Trying to make the markers work properly

--- script.js
+++ script.js
@@ -2,14 +2,22 @@
 
 const addHabitBtn = document.querySelector('.add-habit-btn');
 
+// FIX THIS
 function createHabit() {
+  let goal;
   let habit = window.prompt("Write down a habit that you'd like to work on.");
-  let goal = window.prompt(
-    'How many days per week would you like to work on it?'
-  );
+  if (typeof habit !== 'string') {
+    window.alert('Numbers are not accepted. Please write a goal.');
+  } else {
+    goal = window.prompt(
+      'How many days per week would you like to work on it?'
+    );
+  }
+
   document.querySelector(
     'tbody'
-  ).innerHTML += `<tr><th scope="row">${habit}</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td class="goal-cell">${goal}</td><td></td></tr>`;
+  ).innerHTML += `<tr><th scope="row">${habit}</th><td class="markable-cell"></td><td class="markable-cell"></td><td class="markable-cell"></td><td class="markable-cell"></td><td class="markable-cell"></td><td class="markable-cell"></td><td class="markable-cell"></td><td></td><td class="goal-cell">${goal}</td><td></td></tr>`;
+  console.log(typeof habit);
 }
 
 addHabitBtn.addEventListener('click', function () {
@@ -17,10 +25,11 @@ addHabitBtn.addEventListener('click', function () {
 });
 
 const cells = document.querySelectorAll('.markable-cell');
+
 cells.forEach(function (cell) {
   cell.addEventListener('click', function () {
     if (cell.innerHTML === '') {
-      cell.innerHTML = 'X';
+      cell.innerHTML = '&#9737;';
     } else {
       cell.innerHTML = '';
     }
